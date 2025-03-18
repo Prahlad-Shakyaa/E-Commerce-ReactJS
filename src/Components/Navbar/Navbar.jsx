e6d23456296370e2ee8ba8268dff92a95677a131
Prahlad-Shakyaa <prahladshakya2058@gmail.com.com>
2025-03-18 15:07:21 +0545

    navbar designed

--- src/Components/Navbar/Navbar.jsx
+++ src/Components/Navbar/Navbar.jsx
@@ -1,25 +1,27 @@
-import React from 'react'
+import React, { useState } from 'react'
 import './Navbar.css'
 import logo from '../Assets/logo.png'
 import cart_icon from '../Assets/cart_icon.png'
 
 const Navbar = () => {
+  const [menu,setMenu] = useState("shop")
   return (
     <div className='navbar'>
-        <div className='nav-logo'>
-            <img src="{logo}" alt="" />
-            <p>Shopiing</p>
-        </div>
-        <ul className='nav-menu'>
-            <li>Shop</li>
-            <li>Kids</li>
-            <li>Men</li>
-            <li>Women</li>
-        </ul>
-        <div className='nav-login-cart'>
-            <button>Login</button>
-            <img src="{cart_icon}" alt="" />
-        </div>
+      <div className='nav-logo'>
+        <img src={logo} alt="Logo" />
+        <p>Shopping</p>
+      </div>
+      <ul className='nav-menu'>
+        <li onClick={()=>{setMenu("shop")}}>Shop{menu==="shop"?<hr/>:<></>}</li>
+        <li onClick={()=>{setMenu("kids")}}>Kids{menu==="kids"?<hr/>:<></>}</li>
+        <li onClick={()=>{setMenu("men")}}>Men{menu==="men"?<hr/>:<></>}</li>
+        <li onClick={()=>{setMenu("women")}}>Women{menu==="women"?<hr/>:<></>}</li>
+      </ul>
+      <div className='nav-login-cart'>
+        <button>Login</button>
+        <img src={cart_icon} alt="" />
+        <div className='nav-cart-count'>0</div>
+      </div>
     </div>
   )
 }
